2fc5692fa750d3c635248c495443d6e1583c0cb7
Tony Wu <tonywu93@protonmail.com>
2022-01-07 02:57:29 +1100

    cms

--- sw/test_sensors.py
+++ sw/test_sensors.py
@@ -6,6 +6,5 @@ dev = VariumC1100(0)
 # Wait for initialization (probably should poll...)
 sleep(1)
 
-print(dev.get_register_map_id())
 # Get sensors (Cannot get current without more knowledge of board)
 print("fpga temp (inst, avg, max): " + str(dev.get_fpga_temp()))
