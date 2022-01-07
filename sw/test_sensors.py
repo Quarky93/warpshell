50d78d4feabecf65ecf437c11d17ed6b248e7346
Tony Wu <tonywu93@protonmail.com>
2022-01-07 16:00:26 +1100

    loop test sensors

--- sw/test_sensors.py
+++ sw/test_sensors.py
@@ -5,9 +5,11 @@ from devices.varium_c1100 import VariumC1100
 dev = VariumC1100(0)
 
 # Get sensors (Cannot get current without more knowledge of board)
-print("fpga temp (inst, avg, max): " + str(dev.get_fpga_temp()))
-print("hbm0 temp (inst, avg, max): " + str(dev.get_hbm0_temp()))
-print("hbm1 temp (inst, avg, max): " + str(dev.get_hbm1_temp()))
+while True:
+    print("fpga temp (inst, avg, max): " + str(dev.get_fpga_temp()))
+    print("hbm0 temp (inst, avg, max): " + str(dev.get_hbm0_temp()))
+    print("hbm1 temp (inst, avg, max): " + str(dev.get_hbm1_temp()))
 
-print("vccint voltage (inst, avg, max): " + str(dev.get_vccint_voltagte()))
-print("vccint current (inst, avg, max): " + str(dev.get_vccint_current()))
+    print("vccint voltage (inst, avg, max): " + str(dev.get_vccint_voltagte()))
+    print("vccint current (inst, avg, max): " + str(dev.get_vccint_current()))
+    sleep(0.5)
