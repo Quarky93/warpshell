9af8c0d32ed7aece31ba0c3b8aa3f4fd64889c5a
Vladimir Komendantskiy <komendantsky@gmail.com>
2022-02-08 18:01:45 +0000

    elaboration on the IP core control protocol

--- sw/rust/devices/examples/dev_core.rs
+++ sw/rust/devices/examples/dev_core.rs
@@ -3,9 +3,12 @@ extern crate warp_devices;
 use warp_devices::{dev_core::DevCoreOps, varium_c1100::VariumC1100};
 
 fn main() {
+    env_logger::init();
+
     let mut varium = VariumC1100::new().expect("cannot construct device");
 
-    let x = 8;
-    let result = varium.compute(x).expect("cannot compute result");
-    println!("{}^2 = {}", x, result);
+    let x: u8 = rand::random();
+    let result = varium.compute(x as u32).expect("cannot compute result");
+    println!("0x{:X}^2 = 0x{:X}", x, result);
+    assert_eq!(result, x as u32 * x as u32);
 }
